c6486d0e1b14504c15dd4938d5de158c8886b315
andrybc <canelbandry@gmail.com>
2025-01-10 04:39:23 -0500

    find missing numbers took a minute to redo but eventually got it after I remembered the concept and rememeber to absolute value the number for the index

--- solutions/refresher.py
+++ solutions/refresher.py
@@ -1,11 +1,17 @@
 class Solution(object):
-    def missingNumber(self, nums):
+    def findDisappearedNumbers(self, nums):
         """
         :type nums: List[int]
-        :rtype: int
+        :rtype: List[int]
         """
-        n = len(nums)
-        actual_sum = sum(nums)
-        supposed_sum = (n*(n+1))//2
+        ans = []
         
-        return supposed_sum - actual_sum
+        for i in nums:
+            if nums[abs(i)-1] > 0:
+                nums[abs(i)-1] *= -1
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                ans.append(i+1)
+                
+        return ans
